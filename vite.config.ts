56314368119a74b0a2bf978464284ad50ebb860a
Mateus Goulart <mat_goulart3003@yahoo.com.br>
2025-06-17 12:16:37 -0300

    improvement: att all dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsConfigPaths from 'vite-tsconfig-paths';
+import tailwindcss from '@tailwindcss/vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsConfigPaths()],
+  plugins: [react(), tsConfigPaths(), tailwindcss()],
   server: {
     port: 3060,
   },
