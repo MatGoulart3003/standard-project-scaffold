71d7ac75a2f383d638fb7496b048d2f7dd35c081
Mateus Goulart <mat_goulart3003@yahoo.com.br>
2025-07-03 15:15:04 -0300

    chore: update

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react-swc';
 import tsConfigPaths from 'vite-tsconfig-paths';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  plugins: [react(), tsConfigPaths()],
+  plugins: [react(), tsConfigPaths(), tailwindcss()],
   server: {
     port: 3060,
   },
